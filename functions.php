29b67ddac0ab82225c5e517fd37caf7cbd08d520
unknown <hazem.tantawy@5d-agency.com>
2021-06-29 18:38:46 +0200

    Make enqueue after register styles and scripts for any conditional handling cases

--- functions.php
+++ functions.php
@@ -12,10 +12,11 @@
      * 
      */
     function theme_enqueue_scripts() {
-        // wp_enqueue_style( 'main-stylsheet', get_template_directory_uri() . '/main.css' );
-        wp_enqueue_style( 'theme-stylesheet', get_stylesheet_uri(), [], filemtime( get_template_directory() . '/style.css' ), 'all' );
+        wp_register_style( 'theme-stylesheet', get_stylesheet_uri(), [], filemtime( get_template_directory() . '/style.css' ), 'all' );
+        wp_register_script( 'main-js', get_template_directory_uri() . '/assets/js/main.js', [], filemtime( get_template_directory() . '/assets/js/main.js' ), true  );
 
-        wp_enqueue_script( 'main-js', get_template_directory_uri() . '/assets/js/main.js', [], filemtime( get_template_directory() . '/assets/js/main.js' ), true  );
+        wp_enqueue_style( 'theme-stylesheet' );
+        wp_enqueue_script( 'main-js' );
     }
 
     add_action( 'wp_enqueue_scripts', 'theme_enqueue_scripts' );
